a70c1b564b8e92f389f992163aeae57d37480689
Bryan Bennett <bennettiy12@gmail.com>
2016-08-24 08:21:12 -0400

    Update media.py

--- MovieTrailer/media.py
+++ MovieTrailer/media.py
@@ -5,12 +5,12 @@ class Movie():
     
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+    """ This function initializes space in memory to remember arguments for the Movie class. """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    """ This function initializes space in memory to remember arguments for the Movie class. """
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
     """ This instance method opens the web browser with the correct URL."""
+        webbrowser.open(self.trailer_youtube_url)
